5a97afaa4a9596627083251fe6e0edaff7af1014
Oscar BRUNEAU <obruneau@quanticdream.com>
2025-08-20 19:36:32 +0200

    thing in progress

--- include/trivex.h
+++ include/trivex.h
@@ -1,6 +1,10 @@
 #ifndef TRIVEX_H
 #define TRIVEX_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -9,6 +13,15 @@ enum TVX_Result
 	TVX_RESULT_SUCCESS,
 	TVX_RESULT_COULD_NOT_OPEN_FILE
 };
+
+enum TVX_VertexAttributeFlagBit
+{
+	TVX_VERTEX_ATTRIBUTE_FLAG_BIT_POSITION = 0,
+	TVX_VERTEX_ATTRIBUTE_FLAG_BIT_NORMAL = 1,
+	TVX_VERTEX_ATTRIBUTE_FLAG_BIT_UV = 2
+};
+
+typedef uint8_t TVX_VertexAttributeFlags;
 
 struct TVX_Position
 {
@@ -29,20 +42,23 @@ struct TVX_UV
 	float U;
 	float V;
 };
-
+ 
 struct TVX_Mesh
 {
-	uint32_t positionCount;
-	uint32_t normalCount;
-	uint32_t UVCount;
-	uint32_t attributIndexCount;
+	TVX_VertexAttributeFlags vertexAttributeFlags;
+	uint32_t vertexCount;
 	struct TVX_Position* pPositions;
 	struct TVX_Normal* pNormals;
 	struct TVX_UV* pUVs;
-	uint32_t* pAttributeIndices; // [position / normal / UV] * vertex count
+	uint32_t vertexIndexCount;
+	uint32_t* pVertexIndices;
 };
 
 enum TVX_Result TVX_loadMeshFromOBJ(const char* pPath, struct TVX_Mesh* pMesh);
 void TVX_freeMesh(struct TVX_Mesh mesh);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
